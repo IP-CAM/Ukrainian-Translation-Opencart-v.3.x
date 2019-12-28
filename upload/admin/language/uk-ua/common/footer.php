9c0abdafa0880722eb9af9b97911c227f20fe0de
Sergii Smyrnov <smirnoffs@gmail.com>
2019-12-28 16:26:16 +0100

    Add base ukrainian translation.

---
+++ upload/admin/language/uk-ua/common/footer.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_footer'] 	= '<HR>&copy; 2020-' . date('Y') . ' Всі права захищені.<br> <BR> <a href="https://karkar.com.ua" target="_blank" class="btn btn-primary">Каркар - Магазин для художників.</a>';
+$_['text_version'] 	= 'Version %s (trs-3)';
+
